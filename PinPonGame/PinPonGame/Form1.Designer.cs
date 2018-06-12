ebf103d11ec7778ca54e4c478baae817abb8e655
Nurbüke TEKER <37930894+NurbukeTeker@users.noreply.github.com>
2018-06-12 22:56:15 +0300

    PinPong Games is uploaded
    I have uploaded the all components for open this game in Visual Studio

---
+++ PinPonGame/PinPonGame/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PinPonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fireball = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fireball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(6, 24);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(885, 11);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(6, 482);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(885, 11);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Location = new System.Drawing.Point(65, 206);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(16, 138);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label6.Location = new System.Drawing.Point(824, 206);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(16, 138);
+            this.label6.TabIndex = 5;
+            // 
+            // fireball
+            // 
+            this.fireball.ErrorImage = global::PinPonGame.Properties.Resources.fireball;
+            this.fireball.Image = global::PinPonGame.Properties.Resources.fireball;
+            this.fireball.Location = new System.Drawing.Point(245, 250);
+            this.fireball.Margin = new System.Windows.Forms.Padding(1);
+            this.fireball.Name = "fireball";
+            this.fireball.Size = new System.Drawing.Size(35, 31);
+            this.fireball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fireball.TabIndex = 6;
+            this.fireball.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Image = global::PinPonGame.Properties.Resources.RW_pjg;
+            this.label2.Location = new System.Drawing.Point(886, 0);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(14, 530);
+            this.label2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Image = global::PinPonGame.Properties.Resources.RW_pjg;
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(14, 530);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(903, 539);
+            this.Controls.Add(this.fireball);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_Keydown);
+            ((System.ComponentModel.ISupportInitialize)(this.fireball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox fireball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ PinPonGame/PinPonGame/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PinPonGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+           
+
+        }
+        Random rand = new Random();
+
+        private void Form1_Keydown(object sender, KeyEventArgs e)
+        {
+            int x = label5.Location.X;
+            int y = label5.Location.Y;
+            if(e.KeyCode == Keys.W)
+            {
+                y = y - 20;
+                label5.Location = new Point(x, y);
+
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                y = y + 20;
+                label5.Location = new Point(x, y);
+
+            }//for firstPlayer
+            int a = label6.Location.X;
+            int b = label6.Location.Y;
+            if (e.KeyCode == Keys.Up)
+            {
+                b = b - 20;
+                label6.Location = new Point(a, b);
+
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                b = b + 20;
+                label6.Location = new Point(a, b);
+
+            }//for secondPlayer
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int ball1 = rand.Next(10,14);
+            fireball.Left = fireball.Left + ball1;
+            if (label6.Bottom >= fireball.Top && label6.Top <= fireball.Bottom && label6.Right >= fireball.Left && label6.Left <= fireball.Right)
+            {
+                timer1.Enabled = false; timer2.Enabled = true;
+            }
+         
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int ball1 = rand.Next(10,16);
+            fireball.Left = fireball.Left - ball1;
+            if(label5.Top <= fireball.Bottom && label5.Bottom >= fireball.Top && label5.Left <= fireball.Right && label5.Right>= fireball.Left)
+            {
+                timer2.Enabled = false;  timer3.Enabled = true;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int top1 = rand.Next(10, 10);
+            fireball.Top = fireball.Top + top1;
+            if(fireball.Bottom > label4.Top)
+            {
+                timer3.Enabled = false; timer4.Enabled = true;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            int top1 = rand.Next(10, 15);
+            fireball.Top = fireball.Top - top1;
+
+
+            if(fireball.Top < label3.Bottom)
+            {
+                timer4.Enabled = false;
+     
+                timer5.Enabled = true;
+            }
+           
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            int top1 = rand.Next(10, 15);
+            fireball.Top = fireball.Top - top1;
+
+
+            if (fireball.Left < label1.Right)
+            {
+                timer5.Enabled = false;
+                fireball.Visible = false;
+                MessageBox.Show("Second Player Wim !!");
+            }
+            if(fireball.Right > label2.Left)
+            {
+                timer5.Enabled = false;
+                fireball.Visible = false;
+                MessageBox.Show("First Player Win");
+
+            }
+
+        }
+    }
+}
